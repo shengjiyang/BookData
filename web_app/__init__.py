2ede78aeaa2eb3429dc3ce98171727aa2c7e61e2
shengjiyang <samswank@gmail.com>
2020-05-18 16:35:22 -0600

    Built database, fixed import issue

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,11 +2,20 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
+
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+DATABASE_URI = "sqlite:////Users/samuel/Programming/GitHub/Lambda\ School/Assignments/BookData/book_database.db" # using absolute filepath on Mac (recommended)
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
