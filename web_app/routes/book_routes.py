25a88e531df2ee202fbf4120c5563910abca4da6
shengjiyang <samswank@gmail.com>
2020-05-20 14:30:16 -0600

    Setup homepage to navigate to other pages

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,29 +1,56 @@
 # web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from flask import Blueprint, jsonify, request, render_template, flash, redirect
+from ..models import Book, db
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
     books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
+        {"id": 1, "title": "Dune"},
+        {"id": 2, "title": "Dune Messiah"},
+        {"id": 3, "title": "Children of Dune"},
     ]
     return jsonify(books)
 
 
+
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return render_template("books.html", message="Here are some books!", books=books)
+    # books = [
+    #     {"id": 1, "title": "Dune"},
+    #     {"id": 2, "title": "Dune Messiah"},
+    #     {"id": 3, "title": "Children of Dune"},
+    # ]
+
+    # SELECT * FROM books;
+    book_records = Book.query.all()
+    print(book_records)
+
+    return render_template("books.html", message="Here are some books!", books=book_records)
 
 
 @book_routes.route("/books/new")
 def new_book():
     return render_template("new_book.html")
+
+@book_routes.route("/books/create", methods=["POST"])
+def create_book():
+    print("FORM DATA:", dict(request.form))
+
+    # INSERT INTO books...
+    new_book = Book(title=request.form["title"], author_id=request.form["author_name"])
+    db.session.add(new_book)
+    db.session.commit()
+
+    return redirect("/books")
+
+# @book_routes.route("/books/create", methods=["POST"])
+# def create_book():
+#     print("FORM DATA:", dict(request.form))
+
+#     return jsonify({
+#         "message" : "BOOK CREATED",
+#         "title" : dict(request.form)
+#     })
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,14 +1,13 @@
   # web_app/routes/home_routes.py
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
-def index():
-    x = 2 + 2
-    return f"Hello World! {x}"
+def home_page():
+    return render_template("home_page.html")
 
-@home_routes.route("/about")
-def about():
-    return "About me"
+# @home_routes.route("/about")
+# def about():
+#     return "About me"
